399a6df1341ecaa3d94109cf6366701ec6e6954d
unknown <miguellemes005@gmail.com>
2024-01-11 20:15:03 -0300

    Encrypting password user

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -7,8 +7,12 @@ import (
 	"github.com/migueelzz/crud-go/src/configuration/logger"
 	"github.com/migueelzz/crud-go/src/configuration/validation"
 	"github.com/migueelzz/crud-go/src/controller/model/request"
-	"github.com/migueelzz/crud-go/src/controller/model/response"
+	"github.com/migueelzz/crud-go/src/model"
 	"go.uber.org/zap"
+)
+
+var (
+	UserDomainInterface model.UserDomainInterface
 )
 
 func CreateUser(c *gin.Context) {
@@ -27,15 +31,19 @@ func CreateUser(c *gin.Context) {
 		return
 	}
 
-	response := response.UserResponse{
-		ID:    "test",
-		Email: userRequest.Email,
-		Name:  userRequest.Name,
-		Age:   userRequest.Age,
+	domain := model.NewUserDomain(
+		userRequest.Email,
+		userRequest.Password,
+		userRequest.Name,
+		userRequest.Age,
+	)
+	if err := domain.CreateUser(); err != nil {
+		c.JSON(err.Code, err)
+		return
 	}
 
 	logger.Info("User created successfully",
 		zap.String("journey", "createUser"))
 
-	c.JSON(http.StatusOK, response)
+	c.String(http.StatusOK, "")
 }
